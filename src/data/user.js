fe456e2f43a8e88d92e36dbca1793e8a246d1a69
CAPTAIN-JOB <kiptoojob73@gmail.com>
2024-07-31 00:43:07 +0300

    init

--- src/data/user.js
+++ src/data/user.js
@@ -4,3 +4,5 @@ export const city = "New York";
 export const image = "https://i.imgur.com/mV8PQxj.gif";
 
 // export all three variables as named exports
+// import { username, city, image } from "../data/user";
+// import { username, city, image } from "../data/user";
